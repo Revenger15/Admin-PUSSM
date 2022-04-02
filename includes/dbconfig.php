b5c93b37008d2b6901407923f6dc3549a5e7b6a6
Revenger15 <55306826+Revenger15@users.noreply.github.com>
2022-04-02 13:16:48 +0800

    dbconfig fix
    added error_reporting(0);

--- includes/dbconfig.php
+++ includes/dbconfig.php
@@ -5,6 +5,8 @@
     use Kreait\Firebase\Factory;
     use Kreait\Firebase\ServiceAccount;
 
+    error_reporting(0);
+
     // $serviceAccount = ServiceAccount::fromJsonFile(__DIR__. 'react-ebe8e-firebase-adminsdk-edc3p-3832332702.json');
     $firebase = (new Factory)
         ->withServiceAccount(__DIR__. '/assess-module-firebase-adminsdk-bgqta-e4cd5f3d27.json')
