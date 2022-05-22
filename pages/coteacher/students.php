65e5d50e44cd04d17ca93f4666b975a095ccdadc
Revenger15 <55306826+Revenger15@users.noreply.github.com>
2022-05-22 20:27:46 +0800

    Fixed UI
    Student Active Nav

--- pages/coteacher/students.php
+++ pages/coteacher/students.php
@@ -68,7 +68,7 @@
           </a>
         </li>
         <li class="nav-item">
-          <a class="nav-link text-white" href="#">
+          <a class="nav-link text-white active bg-gradient-faded-dark-vertical" href="#">
             <div class="text-white text-center me-2 d-flex align-items-center justify-content-center">
               <i class="material-icons opacity-10">assignment</i>
             </div>
